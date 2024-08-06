c08c655b3b014a85ee3501d9b0fbe485a51f8733
Katarzyna Olkowska <olkowskakatarzyna2@gmail.com>
2024-08-07 00:21:31 +0200

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from "./logo.svg";
+import logo from "./logo-color.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="imgLogo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
       </header>
+      <section className="HeroSection">
+        <div>
+          <h1>
+            Jeśli potrzebujesz porady prawnej z zakresu prawa pracy lub prawa
+            spółek, wysoce wykwalifikowani specjaliści e-counting służą
+            wsparciem dopasowanym do rodzaju działalności Twojej firmy! Naszym
+            celem jest, aby nasi partnerzy czuli się pewni, że przestrzegają
+            przepisów prawa podczas rozwijania swojego biznesu.
+          </h1>
+        </div>
+      </section>
+      <section className="ProductSection">
+        <h2>Dla każdego przedsiębiorcy</h2>
+        <h3>
+          Niezależnie od tego, czy jesteś początkującym przedsiębiorcą, czy
+          doświadczonym właścicielem firmy, możesz polegać na naszym
+          doświadczeniu i wiedzy w dziedzinie biznesowo- prawnej.
+        </h3>
+      </section>
+      <button className="buttonForm">CHCĘ SKORZYSTAĆ Z WASZEJ WIEDZY</button>
+      <section className="FeatureSection">
+        <div className="Label">
+          <h2>Usługi doradztwa prawnego</h2>
+          <div>
+            - Sporządzanie opinii prawniczej. - Sporządzanie dokumentacji
+            prawnej. - Indywidualne usługi doradztwa prawnego.
+          </div>
+        </div>
+        <div className="Label">
+          <h2>Konsultacje w ramach interpretacji prawa</h2>
+          <div>
+            - Interpretacja przepisów prawnych. - Interpretacja prawa pracy. -
+            Interpretacja prawa gospodarczego.
+          </div>
+        </div>
+      </section>
+      <section className="WhySection">
+        <h2>Dlaczego warto wybrać e-counting?</h2>
+        <div>Dołącz do naszego grona ponad 150 zadowolonych klientów!</div>
+      </section>
+      <button className="buttonForm">CHCĘ Z WAMI WSPÓŁPRACOWAĆ</button>
     </div>
   );
 }
